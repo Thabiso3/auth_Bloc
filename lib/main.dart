6b277e5585cad2eed2a615284ebef38dadc90fe1
Thabiso <102311343+Thabiso3@users.noreply.github.com>
2023-11-01 01:23:33 +0200

    Profile Repository and Profile Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fb_auth_bloc/blocs/auth/auth_bloc.dart';
-import 'package:fb_auth_bloc/blocs/cubit/signup_cubit.dart';
+import 'package:fb_auth_bloc/blocs/cubit/profile_cubit.dart';
 import 'package:fb_auth_bloc/blocs/signin/signin_cubit.dart';
+import 'package:fb_auth_bloc/blocs/signup/signup_cubit.dart';
 import 'package:fb_auth_bloc/pages/home_page.dart';
 import 'package:fb_auth_bloc/pages/signin_page.dart';
 import 'package:fb_auth_bloc/pages/signup_page.dart';
 import 'package:fb_auth_bloc/pages/splash_page.dart';
 import 'package:fb_auth_bloc/repositories/auth_repository.dart';
+import 'package:fb_auth_bloc/repositories/profile_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -29,10 +31,16 @@ class MyApp extends StatelessWidget {
     return MultiRepositoryProvider(
       providers: [
         RepositoryProvider<AuthRepository>(
-            create: (context) => AuthRepository(
-                  firebaseFirestore: FirebaseFirestore.instance,
-                  firebaseAuth: FirebaseAuth.instance,
-                ))
+          create: (context) => AuthRepository(
+            firebaseFirestore: FirebaseFirestore.instance,
+            firebaseAuth: FirebaseAuth.instance,
+          ),
+        ),
+        RepositoryProvider<ProfileRepository>(
+          create: (context) => ProfileRepository(
+            firebaseFirestore: FirebaseFirestore.instance,
+          ),
+        ),
       ],
       child: MultiBlocProvider(
         providers: [
@@ -49,6 +57,11 @@ class MyApp extends StatelessWidget {
           BlocProvider<SignupCubit>(
             create: (context) => SignupCubit(
               authRepository: context.read<AuthRepository>(),
+            ),
+          ),
+          BlocProvider<ProfileCubit>(
+            create: (context) => ProfileCubit(
+              profileRepository: context.read<ProfileRepository>(),
             ),
           ),
         ],
